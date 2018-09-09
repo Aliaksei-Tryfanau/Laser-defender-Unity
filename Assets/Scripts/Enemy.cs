568af4bd646bf9f75b32cc2ba7392010694b21f2
Aleksey Trifonov <altrishka@gmail.com>
2018-09-09 21:22:40 +0300

    Projectiles damage

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,17 +6,43 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     float health = 100f;
+    [SerializeField]
+    float shotCounter;
+    [SerializeField]
+    float minTimeBetweenShots = 0.2f;
+    [SerializeField]
+    float maxTimeBetweenShots = 3f;
+    [SerializeField]
+    GameObject projectile;
+    [SerializeField]
+    float projectileSpeed = 10f;
 
     // Use this for initialization
     void Start()
     {
-
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CountDownAndShoot();
+    }
 
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(projectile, transform.position, Quaternion.identity);
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -28,6 +54,7 @@ public class Enemy : MonoBehaviour
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
+        damageDealer.Hit();
         if (health <= 0)
         {
             Destroy(gameObject);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player")]
     [SerializeField]
     float moveSpeed = 10f;
     [SerializeField]
     float padding = 1f;
+    [SerializeField]
+    int health = 200;
+
+    [Header("Projectile")]
     [SerializeField]
     GameObject laserPrefab;
     [SerializeField]
@@ -32,6 +37,26 @@ public class Player : MonoBehaviour
     {
         Move();
         Fire();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer)
+        {
+            return;
+        }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Fire()
